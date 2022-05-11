85cc63ae92de19bac45825ee3002d651a1f5d972
Mykola <57412569+mykolaKhlopyk@users.noreply.github.com>
2022-05-11 23:54:02 +0300

    Update PersistentRBTree.java
    implemented the class PersistentRBTree

--- Trees/src.java/com/education/mkh/trees/models/PersistentRBTree.java
+++ Trees/src.java/com/education/mkh/trees/models/PersistentRBTree.java
@@ -1,9 +1,103 @@
 package com.education.mkh.trees.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class PersistentRBTree<T extends Comparable<T>>extends RBTree<T> implements TreeFunctionable<T>{
-
+public class PersistentRBTree<T extends Comparable<T>>extends RBTree<T>{
+	private ArrayList<RBTreeNode<T>> roots;
 	
+	public PersistentRBTree() {
+		super();
+		this.roots = new ArrayList<>();
+		roots.add(leaf);
+	}
+	
+	public boolean taskWithTree(T key, TREES_TASKS task) {
+		
+		switch (task) {
+		case INSERT:
+			if (this.search(key)) {
+				return false;
+			}
+			if (this.root!=this.roots.get(this.roots.size()-1)) {
+				deleteOldRootsAndCorrectionNodesParent();
+			}
+			if (!super.insert(key)) {
+				return false;
+			}
+			break;
+		case DELETE:
+			if (!this.search(key)) {
+				return false;
+			}
+			if (this.root!=this.roots.get(this.roots.size()-1)) {
+				deleteOldRootsAndCorrectionNodesParent();
+			}
+			if (!super.delete(key)) {
+				return false;
+			}
+			break;
+		case SEARCH:
+			return search(key);
+		default:
+			break;
+		}
+		clearCopy(root);
+		this.roots.add(root);
+		return true;
+			
+	}
+		
+	public boolean moveRight()  {
+		if (root==null) {
+			return false;
+		}
+		int index = roots.indexOf(root);
+		if (index < roots.size()-1) {
+			this.root = roots.get(index + 1);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean moveLeft()  {
+		if (root==null) {
+			return false;
+		}
+		int index = roots.indexOf(root);
+		if (index > 0) {
+			this.root = roots.get(index - 1);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private void clearCopy(BinaryTreeNode<T> current) {
+		if (!current.isLeaf && current.isCloned) {
+			current.isCloned=false;
+			clearCopy(current.getLeft());
+			clearCopy(current.getRight());
+		}
+	}
+
+	private void deleteOldRootsAndCorrectionNodesParent() {
+		int index=this.roots.indexOf(root);
+	
+		this.roots.subList(index+1, roots.size()).clear();;
+		recursionCorrectionParent(root);
+	}
+	
+	private void recursionCorrectionParent(NodeWithTwoChilds<T> current) {
+		if (!current.getLeft().isLeaf) {
+			current.getLeft().setParent(current);
+			recursionCorrectionParent(current.getLeft());
+		}
+		if (!current.getRight().isLeaf) {
+			current.getRight().setParent(current);
+			recursionCorrectionParent(current.getRight());
+		}
+	}
 
 }
