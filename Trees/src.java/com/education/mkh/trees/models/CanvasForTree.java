27dc2135bea283b201307da755cd413a90b5316b
Mykola <57412569+mykolaKhlopyk@users.noreply.github.com>
2022-05-12 15:14:04 +0300

    Create CanvasForTree.java
    class which have to draw trees in canvas + it implements singleton pattern

---
+++ Trees/src.java/com/education/mkh/trees/models/CanvasForTree.java
@@ -0,0 +1,31 @@
+package com.education.mkh.trees.models;
+
+import javafx.scene.canvas.Canvas;
+
+public class CanvasForTree {
+	Canvas canvas;
+	TreeFunctionable tree;
+	private volatile static CanvasForTree instance;
+	private CanvasForTree() {}
+	private CanvasForTree(Canvas canvas) {
+		this.canvas=canvas;
+	}
+	public CanvasForTree getInstance() {
+		if (instance==null) {
+			synchronized (CanvasForTree.class) {
+				if (instance==null) {
+					instance= new CanvasForTree();
+				}
+			}
+		}
+		return instance;
+	}
+	public void setCanvas(Canvas canvas) {
+		this.canvas=canvas;
+	}
+	public void setTree(TreeFunctionable tree) {
+		this.tree=tree;
+	}
+	
+	
+}
