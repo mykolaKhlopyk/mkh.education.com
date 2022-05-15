025245d252484dfd0d7f144c3e94e9e153e2dd21
Mykola <57412569+mykolaKhlopyk@users.noreply.github.com>
2022-05-15 16:03:00 +0300

    Create ElementsForViewing.java
    create class for controlling classes for viewing

---
+++ Trees/src.java/com/education/mkh/trees/models/ElementsForViewing.java
@@ -0,0 +1,37 @@
+package com.education.mkh.trees.models;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ElementsForViewing implements Subject{
+	LinkedList<Observer> observers;
+	TreeFunctionable tree;
+	public ElementsForViewing() {
+		this.observers=new LinkedList<Observer>();
+		tree=null;
+	}
+	
+	@Override
+	public void registerObserver(Observer observer) {
+		observers.add(observer);
+	}
+
+	@Override
+	public void removeObserver(Observer observer) {
+		observers.remove(observer);
+	}
+
+	@Override
+	public void notifyObservers() {
+		Iterator<Observer> it=observers.iterator();
+		while (it.hasNext()) {
+			Observer observer = (Observer) it.next();
+			observer.update(tree);			
+		}
+	}
+
+	public void setTree(TreeFunctionable tree) {
+		this.tree=tree;
+	}
+}
