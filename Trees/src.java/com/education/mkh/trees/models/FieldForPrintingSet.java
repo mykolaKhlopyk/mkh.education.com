52f2168d7479c55655b45cfb5cf4e6216eef8c0f
Mykola <57412569+mykolaKhlopyk@users.noreply.github.com>
2022-05-15 16:04:49 +0300

    Create FieldForPrintingSet.java
    observer which has to print set, read set from tree by using Iterator

---
+++ Trees/src.java/com/education/mkh/trees/models/FieldForPrintingSet.java
@@ -0,0 +1,50 @@
+package com.education.mkh.trees.models;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
+
+public class FieldForPrintingSet implements Observer{
+	TextField mainField;
+	TreeFunctionable tree;
+	private volatile static FieldForPrintingSet instance;
+	private FieldForPrintingSet() {}
+	private FieldForPrintingSet(TextField mainField) {
+		this.mainField=mainField;
+	}
+	public static FieldForPrintingSet getInstance() {
+		if (instance==null) {
+			synchronized (CanvasForTree.class) {
+				if (instance==null) {
+					instance= new FieldForPrintingSet();
+				}
+			}
+		}
+		return instance;
+	}
+	public void setTextField(TextField textField) {
+		this.mainField = textField;
+		
+	}
+	public void setTree(TreeFunctionable tree) {
+		this.tree=tree;
+	}
+	@Override
+	public void update(TreeFunctionable tree) {
+		setTree(tree);
+		if (tree==null) {
+			mainField.setText("Set is empty");
+			return;
+		}
+		List list=tree.toList();		
+		if (list == null || list.isEmpty()) {
+			mainField.setText("Set is empty");
+		}else{
+			mainField.setText(list.toString());
+		}
+	}
+	
+}
