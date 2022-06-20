40df2d162348a96553db5fc50b50d6aa98208955
Mykola <57412569+mykolaKhlopyk@users.noreply.github.com>
2022-06-20 11:54:32 +0300

    more using obsrver pattern
    user can choose showing set or not

---
+++ Trees/src.java/com/education/mkh/trees/models/CommandForList.java
@@ -0,0 +1,37 @@
+package com.education.mkh.trees.models;
+
+import java.util.List;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+
+public class CommandForList<T> implements Command<T> {
+	private TreeFunctionable tree;
+	private Button but;
+	private ElementsForViewing elem;
+
+	public CommandForList(TreeFunctionable<T> tree, Button but, ElementsForViewing elem) {
+		this.tree = tree;
+		this.but = but;
+		this.elem = elem;
+	}
+
+	@Override
+	public void execute(String key) {
+		if (but.getText().equals("У список")) {
+			if (tree instanceof PersistentRBTreeAdapter) {
+				((PersistentRBTreeAdapter) tree).correctionParent();
+			}
+			List list = tree.toList();
+			elem.registerObserver(FieldForPrintingSet.getInstance());
+			elem.notifyObservers();
+			but.setText("Без списку");
+		}else{
+			elem.removeObserver(FieldForPrintingSet.getInstance());
+			FieldForPrintingSet.getInstance().setEmpty();
+			but.setText("У список");
+		}
+	}
+
+}
--- Trees/src.java/com/education/mkh/trees/models/FieldForPrintingSet.java
+++ Trees/src.java/com/education/mkh/trees/models/FieldForPrintingSet.java
@@ -7,44 +7,63 @@ import java.util.List;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.TextField;
 
-public class FieldForPrintingSet implements Observer{
+public class FieldForPrintingSet implements Observer {
 	private TextField mainField;
 	private TreeFunctionable tree;
 	private volatile static FieldForPrintingSet instance;
-	private FieldForPrintingSet() {}
-	private FieldForPrintingSet(TextField mainField) {
-		this.mainField=mainField;
+
+	private FieldForPrintingSet() {
 	}
+
+	private FieldForPrintingSet(TextField mainField) {
+		this.mainField = mainField;
+	}
+
 	public static FieldForPrintingSet getInstance() {
-		if (instance==null) {
+		if (instance == null) {
 			synchronized (CanvasForTree.class) {
-				if (instance==null) {
-					instance= new FieldForPrintingSet();
+				if (instance == null) {
+					instance = new FieldForPrintingSet();
 				}
 			}
 		}
 		return instance;
 	}
+
 	public void setTextField(TextField textField) {
 		this.mainField = textField;
-		
+
 	}
+
 	public void setTree(TreeFunctionable tree) {
-		this.tree=tree;
+		this.tree = tree;
 	}
+
 	@Override
 	public void update(TreeFunctionable tree) {
 		setTree(tree);
-		if (tree==null) {
+		if (tree == null) {
 			mainField.setText("Set is empty");
 			return;
 		}
-		List list=tree.toList();		
-		if (list == null || list.isEmpty()) {
-			mainField.setText("Set is empty");
-		}else{
-			mainField.setText(list.toString());
-		}
+		String result="";
+		//Runnable r = () -> {
+			List list = tree.toList();
+			if (list == null || list.isEmpty()) {
+				result="Set is empty";
+				//mainField.setText("Set is empty");
+			} else {
+				result = list.toString();
+				//mainField.setText(list.toString());
+			}
+			synchronized (mainField) {
+				mainField.setText(result);
+			}
+		//};
+		//new Thread(r).start();
 	}
-	
+
+	void setEmpty() {
+		this.mainField.setText("");
+	}
 }
